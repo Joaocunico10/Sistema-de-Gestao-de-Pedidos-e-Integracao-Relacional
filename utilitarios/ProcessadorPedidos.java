f0ec0c38a7e0a01818f08a110d6dc202ea20822a
Joao Paulo <engs-joaocunico@camporeal.edu.br>
2026-05-28 10:10:43 -0300

    Colocando os imports

--- utilitarios/ProcessadorPedidos.java
+++ utilitarios/ProcessadorPedidos.java
@@ -1,5 +1,12 @@
 package utilitarios;
 
+import dados.RepositorioPedido;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.util.List;
+import modelos.Pedido;
+import modelos.StatusPedido;
+
 public class ProcessadorPedidos extends Thread {
 
 
