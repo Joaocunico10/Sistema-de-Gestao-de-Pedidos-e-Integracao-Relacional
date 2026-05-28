dabbb807e31f9e15dcb4cd1eaab958d20f1538ba
Luis Molon <engs-luismolon@camporeal.edu.br>
2026-05-28 12:32:01 -0300

    fiz a parte De validacao

---
+++ utilitarios/ValidacaoException.java
@@ -0,0 +1,11 @@
+package utilitarios;
+
+public class ValidacaoException extends RuntimeException {
+
+    public ValidacaoException(String mensagem) {
+        super(mensagem);
+    }
+    public ValidacaoException(String mensagem, Throwable causa) {
+        super(mensagem, causa);
+    }
+}
